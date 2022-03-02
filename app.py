1681d0789b243c7af3b650e7f614ea8e2bf46036
mathildapurr <23065951+mathildapurr@users.noreply.github.com>
2022-03-02 14:49:56 -0800

    edit message

--- app.py
+++ app.py
@@ -21,9 +21,9 @@ def message_customer(message, ack, say):
     # send different links based on channel. Message fields here: https://api.slack.com/events/message
     text = f""
     if message['channel'] == graphing_id:
-        text = f":wave: Hey there <@{message['user']}>! Please take a moment to *go through <https://datadoghq.atlassian.net/wiki/spaces/TS/pages/2245853726/Escalation+workflow+for+troubleshooting+dashboard+dataviz+notebooks+issues|our escalation workflow wiki> first*. When you are finished, let us know if you still need help by:"
+        text = f":wave: Hey there <@{message['user']}>! Please take a moment to *go through <https://datadoghq.atlassian.net/wiki/spaces/TS/pages/2245853726/Escalation+workflow+for+troubleshooting+dashboard+dataviz+notebooks+issues|our escalation workflow wiki> first*. When you are finished, react to your own post with:"
     if message['channel'] == aaa_id:
-        text = f":wave: Hey there <@{message['user']}>! Please take a moment to *go through <https://datadoghq.atlassian.net/wiki/spaces/TS/pages/2260729945/Escalation+workflow+for+AAA+Authentication+Access+Control+and+Auditability+Issues|our escalation workflow wiki> first*. When you are finished, let us know if you still need help by:"
+        text = f":wave: Hey there <@{message['user']}>! Please take a moment to *go through <https://datadoghq.atlassian.net/wiki/spaces/TS/pages/2260729945/Escalation+workflow+for+AAA+Authentication+Access+Control+and+Auditability+Issues|our escalation workflow wiki> first*. When you are finished, react to your own post with:"
 
     if 'thread_ts' not in message:
         # say() sends a message to the channel where the event was triggered
@@ -40,7 +40,7 @@ def message_customer(message, ack, say):
                     "type": "section",
                     "text": {
                         "type": "mrkdwn",
-                        "text": "• React to your own post with :white_check_mark: if you no longer need help\n• React to your own post with :hand: if you still need help\n"
+                        "text": "• :white_check_mark: if you no longer need help\n• :hand: if you still need help\n"
                     }
                 }
             ],
